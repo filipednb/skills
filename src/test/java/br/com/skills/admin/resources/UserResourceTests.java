bfb336cb1d1e00a8ce51e9d62aa5c700ead2534d
filipednb <filipednb@gmail.com>
2019-01-05 10:51:28 -0200

    Java 10

--- src/test/java/br/com/skills/admin/resources/UserResourceTests.java
+++ src/test/java/br/com/skills/admin/resources/UserResourceTests.java
@@ -1,7 +1,13 @@
 package br.com.skills.admin.resources;
 
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class UserResourceTests {
 }
